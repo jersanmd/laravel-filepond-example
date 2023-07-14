273c857760381ac171ccad63c32e950f43bd55d3
Jersan Balago <1stjersan@gmail.com>
2023-07-14 14:19:41 +0800

    Update: Only accept PDF files.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,7 @@
 
   <title>Learn FilePod</title>
   <meta name="csrf-token" content="{{ csrf_token() }}">
-
-
+  
     <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
@@ -63,15 +62,23 @@
 
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-
+  <script src="https://unpkg.com/filepond-plugin-file-validate-size/dist/filepond-plugin-file-validate-size.js"></script>
+  <script src="https://unpkg.com/filepond-plugin-file-validate-type/dist/filepond-plugin-file-validate-type.js"></script>
   <script src="https://unpkg.com/filepond@^4/dist/filepond.js"></script>
 
   <script>
     // Get a reference to the file input element
     const inputElement = document.querySelector('input[type="file"]');
 
+    FilePond.registerPlugin(
+      FilePondPluginFileValidateType,
+      FilePondPluginFileValidateSize,
+    );
+
     // Create a FilePond instance
-    const pond = FilePond.create(inputElement);
+    const pond = FilePond.create(inputElement, {
+      acceptedFileTypes: ['application/pdf']
+    });
 
     FilePond.setOptions({
       server: {
